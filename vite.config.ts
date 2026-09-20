10ec124617ed37f10a1c7af365d3a995e24e0164
Where Is The Next Spot <webgrowthsg@gmail.com>
2026-09-20 13:59:09 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-single-file'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   server: {
-    middlewareMode: true,
     headers: {
       'Cache-Control': 'public, max-age=31536000, immutable'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
     }
   }
 })
